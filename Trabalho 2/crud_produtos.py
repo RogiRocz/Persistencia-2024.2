ed3b7b3f749f9c7128e76e0ed9e1b1c5fca4a976
Rogi Rocz <rogirocz@gmail.com>
2025-01-22 18:24:20 -0300

    create an crud for Produtos

---
+++ Trabalho 2/crud_produtos.py
@@ -0,0 +1,101 @@
+from decimal import Decimal
+from fastapi import FastAPI, Depends, HTTPException
+from http import HTTPStatus
+from sqlalchemy.orm import Session
+from db_models import Clientes, Estoque, Produtos, Vendas, Fornecedores
+from db_connect import get_db
+from sqlalchemy.exc import SQLAlchemyError
+from pydantic import BaseModel, Field
+
+class ProdutoPy(BaseModel):
+    nome: str
+    valor_unitario: Decimal = Field(max_digits=10, decimal_places=2)
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return "Ola: Mundo"
+
+@app.get('/produtos')
+def get_all_produtos(db: Session = Depends(get_db)):
+    return db.query(Produtos).order_by(Produtos.ID_Produto).all()
+
+@app.get('/produtos/{id_produto}')
+def get_produto(id_produto: int, db: Session = Depends(get_db)):
+    if not id_produto:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='ID inválido')    
+    
+    try:
+        query = db.query(Produtos).filter_by(ID_Produto = id_produto).first() 
+        if query is None:
+            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='ID não encontrado')
+    except HTTPException as e:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=f'Erro ao retornar o produto. Erro: {str(e)}')
+    return query
+
+@app.post('/produtos')
+def create_produto(produto_req: ProdutoPy, db: Session = Depends(get_db)):
+    if not produto_req.nome:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,detail="O nome do produto não pode estar vazio.")
+    if produto_req.valor_unitario <= 0:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,detail="O valor unitário deve ser maior que zero.")
+    
+    try:
+        produto = Produtos(nome=produto_req.nome, valor_unitario=produto_req.valor_unitario)
+        db.add(produto)
+        db.commit()
+        db.refresh(produto)
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f"Erro ao criar produto no banco de dados: {str(e)}")
+    else:
+        return produto
+
+@app.put('/Produtos/{id_produto}')
+def update_produto(id_produto: int, produto_req: ProdutoPy, db: Session = Depends(get_db)):
+    if not id_produto:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='ID inválido')
+    
+    try:
+        produto_db = db.query(Produtos).filter_by(ID_Produto = id_produto).first()
+        if produto_db is None:
+            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='ID não encontrado')
+        
+        produto_antigo = {
+            "ID_Produto": produto_db.ID_Produto,
+            "nome": produto_db.nome,
+            "valor_unitario": produto_db.valor_unitario
+        }
+        
+        if produto_db.nome is not None:
+            produto_db.nome = produto_req.nome
+        if produto_db.valor_unitario is not None:
+            produto_db.valor_unitario = produto_req.valor_unitario
+            
+        db.commit()
+        db.refresh(produto_db)
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f"Erro ao atualizar o produto no banco de dados: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f"Erro ao atualizar o produto no banco de dados: {str(e)}")
+    else:
+        return produto_antigo
+
+@app.delete('/Produtos/{id_produto}')
+def delete_produto(id_produto: int, db: Session = Depends(get_db)):
+    if not id_produto:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='ID inválido')
+    
+    try:
+        del_produto = db.query(Produtos).filter_by(ID_Produto = id_produto).first()
+        if del_produto is None:
+            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='ID não encontrado')
+        del_produto.delete(synchronize_session=False)
+        db.commit()        
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f"Erro ao excluir o produto no banco de dados: {str(e)}")
+    else:
+        return del_produto
