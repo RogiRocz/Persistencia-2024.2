6d7a47e7bfd399aca9feec043c04133ee515e144
Rogi Rocz <rogirocz@gmail.com>
2025-01-29 15:06:56 -0300

    update logical programming

--- Trabalho 3/models.py
+++ Trabalho 3/models.py
@@ -2,22 +2,23 @@ from odmantic import Model, Field, Reference
 from typing import Optional, List
 from pydantic import field_validator
 from decimal import Decimal
+import re
 
 class Produtos(Model):
-    codigo_barras: str = Field(unique=True, description='O código de barras é único para prdouto')
+    codigo_barras: str = Field(unique=True, description='O código de barras é único para produto')
     nome: str
     valor_unitario: Decimal = Field(ge=0.00, description='O valor_unitario tem que ser positivo')
     
-class Cliente(Model):
+class Clientes(Model):
     forma_pagamento: str
     programa_fidelidade: Optional[str] = None
     
 class ItemVenda(Model):
     produto: Produtos = Reference(key_name='id_produto')
-    quantidade: int = Field(ge=0, description='A quantidade tem que ser positiva')
+    quantidade: int = Field(ge=1, description='A quantidade tem que ser positiva')
     
 class Vendas(Model):
-    cliente: Cliente = Reference(key_name='id_cliente')
+    cliente: Clientes = Reference(key_name='id_cliente')
     produtos: List[ItemVenda]
     valor_total: Decimal = Field(default=Decimal('0.00'))
     
@@ -25,7 +26,10 @@ class Vendas(Model):
         total = Decimal('0.00')
         for item in self.produtos:
             produto = item.produto
-            total += produto.valor_unitario * item.quantidade
+            if not produto.valor_unitario:
+                raise ValueError(f'O valor_unitario de {produto.codigo_barras} não pode ser nulo')
+            else:
+                total += produto.valor_unitario * item.quantidade
             
         return total
     
@@ -43,14 +47,22 @@ class Vendas(Model):
     
 class Fornecedores(Model):
     nome: str
-    cnpj: str = Field(regex=r'^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$', description="Formato de CNPJ inválido")
+    cnpj: str = Field(unique=True)
     endereco: str
+    
+    @field_validator("cnpj")
+    @classmethod
+    def validar_cnpj(cls, valor: str) -> str:
+        padrao_cnpj = r"^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$"
+        if not re.match(padrao_cnpj, valor):
+            raise ValueError("CNPJ inválido! Use o formato 00.000.000/0000-00")
+        return valor
     
 class ProdutosFornecidos(Model):
     produto: Produtos = Reference(key_name='id_produto')
     fornecedor : Fornecedores = Reference(key_name='id_fornecedor')
     quantidade: int = Field(ge=0)
-    custo_unidade: Decimal = Field(ge=0.00)
+    custo_unidade: Decimal = Field(ge=0.00, default=Decimal('0.00'))
     
     @field_validator('custo_unidade', mode='after')
     @classmethod
