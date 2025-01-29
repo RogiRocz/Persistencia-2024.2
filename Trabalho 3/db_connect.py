a60a7ad4a75184a564efba58d2e3898786ffeda4
Rogi Rocz <rogirocz@gmail.com>
2025-01-29 15:06:17 -0300

    update logical programming

--- Trabalho 3/db_connect.py
+++ Trabalho 3/db_connect.py
@@ -1,5 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
+import asyncio
 import models
 import yaml
 import json
@@ -32,29 +33,29 @@ def get_data(file):
     
     
 async def create_produtos(file):
-    db = get_db()
+    db = engine
     data = get_data(file)
     input = []
     
     for produto in data:
-        element = models.Produtos(nome=produto['nome'], valor_unitario=produto['valor_unitario'], codigo_barras=produto['codigo_barras'])
+        element = models.Produtos(nome=produto['nome'], valor_unitario=str(produto['valor_unitario']), codigo_barras=produto['codigo_barras'])
         input.append(element)
     
     await db.save_all(input)
 
 async def create_clientes(file):
-    db = get_db()
+    db = engine
     data = get_data(file)
     input = []
     
     for cliente in data:
-        element = models.Cliente(forma_pagamento=cliente['forma_pagamento'], programa_fidelidade=cliente['programa_fidelidade'])
+        element = models.Clientes(forma_pagamento=cliente['forma_pagamento'], programa_fidelidade=cliente['programa_fidelidade'])
         input.append(element)
     
     await db.save_all(input)
 
 async def create_fornecedores(file):
-    db = get_db()
+    db = engine
     data = get_data(file)
     input = []
     
@@ -65,34 +66,47 @@ async def create_fornecedores(file):
     await db.save_all(input)
 
 async def create_produtos_fornecidos(file):
-    db = get_db()
+    db = engine
     data = get_data(file)
     input = []
     
     for pf in data:        
         produto_ref = await db.find_one(models.Produtos, models.Produtos.codigo_barras == pf['produto'])
-        fornecedor_ref = await db.find_one(models.Fornecedores, models.Fornecedores.name == pf['fornecedor'])
+        if not produto_ref:
+            print(f'create_produtos_fornecidos - Produto não encontrado: {pf['produto']}')
+            continue
+        if not pf['custo_unidade']:
+            print(f'create_produtos_fornecidos - O custo_unidade é nulo: {pf['produto']}')
+            continue
+        fornecedor_ref = await db.find_one(models.Fornecedores, models.Fornecedores.cnpj == pf['fornecedor'])
+        if not fornecedor_ref:
+            print(f'create_produtos_fornecidos - Fornecedor não encontrado: {pf['fornecedor']}')
+            continue
         
-        element = models.ProdutosFornecidos(produto=produto_ref, fornecedor=fornecedor_ref, quantidade=pf['quantidade'], custo_unidade=pf['custo_unidade'])
-        input.append(element)
+        if produto_ref and fornecedor_ref and pf['custo_unidade']:
+            element = models.ProdutosFornecidos(produto=produto_ref, fornecedor=fornecedor_ref, quantidade=pf['quantidade'], custo_unidade=str(pf['custo_unidade']))
+            input.append(element)
     
     await db.save_all(input)
     
 async def create_estoque(file):
-    db = get_db()
+    db = engine
     data = get_data(file)
     input = []
     
     for estoque in data:        
         produto_ref = await db.find_one(models.Produtos, models.Produtos.codigo_barras == estoque['produto'])
-                
-        element = models.Estoque(produto=produto_ref, quantidade=estoque['quantidade'], validade_dias=estoque['validade_dias'])
-        input.append(element)
+        if not produto_ref:
+            print(f'create_estoque - Produto não encontrado: {estoque['produto']}')
+            continue
+        else:
+            element = models.Estoque(produto=produto_ref, quantidade=estoque['quantidade'], validade_dias=estoque['validade_dias'])
+            input.append(element)
     
     await db.save_all(input)
     
 async def create_vendas(file):
-    db = get_db()
+    db = engine
     data = get_data(file)
     input = []
     
@@ -100,17 +114,29 @@ async def create_vendas(file):
         produtos_ref = []
         cliente_ref = object
                 
-        cliente_ref = await db.find_one(models.Cliente, models.Cliente.programa_fidelidade == venda['cliente'])
+        cliente_ref = await db.find_one(models.Clientes, models.Clientes.programa_fidelidade == venda['cliente'])
         for produto in venda['produtos']:
-            p_ref = await db.find_one(models.Produtos, models.Produtos.codigo_barras == produto['codigo_barras'])
-            produtos_ref.append(p_ref)
-            
-        element = models.Vendas(cliente=cliente_ref, produtos=produtos_ref)
-        input.append(element)
+            p_ref = await db.find_one(models.Produtos, models.Produtos.codigo_barras == produto['produto'])
+            if not p_ref:
+                print(f'create_vendas - Produto não encontrado: {produto['produto']}')
+                continue
+            elif not p_ref.valor_unitario:
+                print(f'create_vendas - O valor_unitario é nulo: {produto['produto']}')
+                continue
+            else:
+                item_venda = models.ItemVenda(produto=p_ref, quantidade=produto['quantidade'])
+                produtos_ref.append(item_venda)
+        
+        if cliente_ref:
+            element = models.Vendas(cliente=cliente_ref, produtos=produtos_ref)
+            element.valor_total = element.calcular_valor_total()
+            input.append(element)
+            await db.save_all(input)
+        else:
+            print(f'create_vendas - Clientes não encontrado: {cliente_ref}')
     
-    await db.save_all(input)
 
-def popular_db():
+async def popular_db():
     config = get_yaml_config()
     db_populate = config['db-populate']
     folder_path = os.path.join(curr_dir(), db_populate['folder'])
@@ -120,19 +146,19 @@ def popular_db():
         
         try:        
             if name == 'data_produtos':
-                create_produtos(file)
+                await create_produtos(file)
             if name == 'data_clientes':
-                create_clientes(file)
+                await create_clientes(file)
             if name == 'data_fornecedores':
-                create_fornecedores(file)
+                await create_fornecedores(file)
             if name == 'data_produtos_fornecidos':
-                create_produtos_fornecidos(file)
+                await create_produtos_fornecidos(file)
             if name == 'data_estoque':
-                create_estoque(file)
+                await create_estoque(file)
             if name == 'data_vendas':
-                create_vendas(file)
+                await create_vendas(file)
         except Exception as e:
-            print(f'Error ao inserir os dados do arquivo {name}. Erro: {e}')
+            print(f'Error ao inserir os dados do arquivo {name}. Erro: {str(e)}')
 
 if __name__ == '__main__':
-    popular_db()
+    asyncio.run(popular_db())
